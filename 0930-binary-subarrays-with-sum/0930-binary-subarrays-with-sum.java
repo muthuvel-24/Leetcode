f692841721258f75ed46de517d4131a1ebc510b4
muthuvel-24 <msmuthuvel2004@gmail.com>
2026-08-01 15:13:06 +0530

    Time: 2 ms (61.96%), Space: 50.9 MB (65.53%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return muthu(nums,goal)-muthu(nums,goal-1);
+    }
+     public static int muthu(int nums[],int goal) 
+     {
+        if(goal<0) return 0;
+       int l=0;
+       int r=0;
+       int sum=0;
+       int count=0;
+       while(r<nums.length)
+       {
+        sum+=nums[r];
+         while(sum>goal)
+         {
+                sum=sum-nums[l];
+                l++;
+         }
+         count=count+r-l+1;
+         r++;
+       }
+       return count;
+    }
+}
