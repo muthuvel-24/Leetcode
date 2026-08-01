9ec38780b11c2e0f628c5e48db4bc1f77875ee4e
muthuvel-24 <msmuthuvel2004@gmail.com>
2026-08-01 15:04:06 +0530

    Time: 2 ms (20.81%), Space: 43.3 MB (85.91%) - LeetHub

---
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int vowelConsonantScore(String s) {
+       int v=0;
+       int c=0;
+       for(int i=0;i<s.length();i++)
+       {
+        char ch=s.charAt(i);
+        if("aeiou".indexOf(ch)!=-1)
+        {
+            v++;
+        }
+        if("bcdfghjklmnpqrstvwxyz".indexOf(ch)!=-1)
+        {
+            c++;
+        }
+       } 
+       if(c==0||v==0) return 0;
+       return (int)Math.floor(v/c);
+    }
+}
