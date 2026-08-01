a5e36cc87c3d728ce4ad5043e6811ed7b9be4524
muthuvel-24 <msmuthuvel2004@gmail.com>
2026-08-01 14:29:42 +0530

    Time: 1 ms (100%), Space: 43.5 MB (45.38%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxPower(String s) {
+        int count=1;
+        int max=1;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(s.charAt(i)==s.charAt(i+1))
+            {
+                count++;
+                max=Math.max(max,count);
+            }
+            else {
+                max=Math.max(max,count);
+                count=1;
+            }
+        }
+        return max;
+    }
+}
