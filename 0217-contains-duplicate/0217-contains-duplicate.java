e52ff674b094a4d9b754171ebffd5de7223b074b
muthuvel-24 <msmuthuvel2004@gmail.com>
2026-08-05 23:10:42 +0530

    Time: 25 ms (16.75%), Space: 76.5 MB (99.17%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int count=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1])  count=1;
+        }return count ==1;
+    }
+}
